69f451531b9d00572fcd5422c58f9911f5d18b66
Daksh <54631466+daksh931@users.noreply.github.com>
2023-08-05 22:08:26 +0530

    Update SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -3,19 +3,27 @@ using namespace std;
 
 void bubblesort(int arr[] , int len)
 {
-
-    for ( int i=0 ; i<len; i++)
-    {
-        for( int j=1 ; j<=len-1; j++ )
+        int min_idx;
+    for ( int i=0 ; i<len-1; i++)
+    {   min_idx=i;
+        for (int j = i+1; j <len ; j++)
         {
-            if (arr[j] < arr[j-1])
+            if(arr[j] < arr[min_idx])
             {
-                int temp = arr[j];
-                arr[j] = arr[j-1];
-                arr[j-1] = temp;
+                min_idx = j;    
             }
-        } 
+            // cout<<"value of i "<<i<< " -- Val  of j "<<j<<endl;
+        }
+        if(min_idx != i) 
+        {
+
+            int temp = arr[i];
+            arr[i] = arr[min_idx];
+            arr[min_idx] = temp;
+        }
+        
     }
+
     
 }
 
@@ -29,7 +37,7 @@ void printarr( int arr[] , int len )
 
 int main()
 {
-    int arr[] = {5,-1};
+    int arr[] = {5,-1,8,-12,87,7,61};
     int len = sizeof(arr)/sizeof(arr[0]);
     
     bubblesort(arr , len);
